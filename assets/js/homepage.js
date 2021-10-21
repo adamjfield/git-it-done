967199b10d72e35643fd0cca494ef91385c45e5e
Adam Field <adamjfield@gmail.com>
2021-10-20 23:20:20 -0500

    add language search

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepo = function (user) {
   // format the github api url
@@ -25,6 +26,23 @@ var getUserRepo = function (user) {
     });
 };
 
+var getFeaturedRepos = function (language) {
+  var apiUrl =
+    "https://api.github.com/search/repositories?q=" +
+    language +
+    "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: GitHub User Not Found");
+    }
+  });
+};
+
 var formSubmitHandler = function (event) {
   event.preventDefault();
   var username = nameInputEl.value.trim();
@@ -38,6 +56,16 @@ var formSubmitHandler = function (event) {
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+var buttonClickHandler = function (event) {
+  var language = event.target.getAttribute("data-language");
+  if (language) {
+    getFeaturedRepos(language);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+};
 
 var displayRepos = function (repos, searchTerm) {
   console.log(repos);
@@ -92,3 +120,5 @@ var displayRepos = function (repos, searchTerm) {
     repoContainerEl.appendChild(repoEl);
   }
 };
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);

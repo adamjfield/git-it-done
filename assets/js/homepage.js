10f0a9088ecd1eb1fc56631c89ef8f2ea6639e00
Adam Field <adamjfield@gmail.com>
2021-10-20 22:02:06 -0500

    finish list issue page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,20 +9,20 @@ var getUserRepo = function (user) {
 
   // make a request to the url
   fetch(apiUrl)
-  .then(function (response) {
+    .then(function (response) {
       // request was successful
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayRepos(data, user);
-      });
-    } else {
-      alert("Error: Github User Not Found");
-    }
-  })
-  .catch(function(error) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayRepos(data, user);
+        });
+      } else {
+        alert("Error: Github User Not Found");
+      }
+    })
+    .catch(function (error) {
       // Notice this `.catch()` getting chained into the end of the `.then()` method
       alert("Unable to connect to GitHub");
-  })
+    });
 };
 
 var formSubmitHandler = function (event) {
@@ -59,8 +59,9 @@ var displayRepos = function (repos, searchTerm) {
     var repoName = repos[i].owner.login + "/" + repos[i].name;
 
     // create a container for each repo
-    var repoEl = document.createElement("div");
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
